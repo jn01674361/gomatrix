5016ed49f9b4aae9e69cdb3e66258f0907f6338f
jn01674361 <joar.nykvist@gmail.com>
2019-07-17 00:30:42 +0200

    Add matrix product and matrix printing

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import(
-    "fmt"
+    //"fmt"
     gomatrix "gomatrix"
 )
 
 func main() {
-    mat := gomatrix.NewMatrix(2,2)
-    fmt.Println(mat)
+    
+    A := gomatrix.Matrix{
+        Matrix: [][]float64{
+            {1.0, 2.0},
+            {3.0, 4.0},
+        },
+    }
+    B := gomatrix.Matrix{
+        Matrix: [][]float64{
+            {5.0, 6.0, 7.0},
+            {8.0, 9.0, 10.0},
+        },
+    }
+
+    C := gomatrix.MatMul(A,B)
+
+    gomatrix.PrintMat(A)
+    gomatrix.PrintMat(B)
+    gomatrix.PrintMat(C)
+
 }
--- src/gomatrix/gomatrix.go
+++ src/gomatrix/gomatrix.go
@@ -1,19 +1,80 @@
 package gomatrix
 
+import "fmt"
+
 type Matrix struct{
 	Matrix [][]float64
 } 
-
+// returns the number of rows of a matrix
+// in: the matrix
+/// out: number of rows
+func Rows(A Matrix) int {
+	return len(A.Matrix)
+}
+// returns the number of columns of a matrix
+// in: the matrix
+/// out: number of columns
+func Cols(A Matrix) int {
+	var cols int
+	if len(A.Matrix) > 0 {
+		cols = len(A.Matrix[0])
+	} else {
+		cols = 0
+	}
+	return cols
+}
+// returns a matrix with m rows and n columns, filled with zeros
+// in: number of rows, columns
+/// out: matrix object
 func NewMatrix(m,n int) Matrix {
-
 	mat := make([][]float64, m)
-
 	for row:=0; row<m; row++{
 		mat[row] = make([]float64, n)
 	}
-
 	ret := Matrix{
 		Matrix: mat,
 	}
 	return ret
+}
+// returns the matrix product of two matrices
+// in: two matrices 
+// out: the matrix product of the matrices as a matrix object, 
+// 		if the matrices can be multiplied in the supplied order.
+func MatMul(A,B Matrix) Matrix {
+	var m, n1, n2, p int
+	m = Rows(A)
+	n1 = Cols(A)
+	n2 = Rows(B)
+	p = Cols(B)
+	if n1 != n2 {
+		return NewMatrix(0,0)
+	} else if !(m > 0 && n1 > 0 && n2 > 0 && p > 0) {
+		return NewMatrix(0,0)
+	} else{
+		C := NewMatrix(m,p)
+		for i := 0; i < m; i++ {
+			for j := 0; j < p; j++ {
+				for k := 0; k < n1; k++ {
+					C.Matrix[i][j] += A.Matrix[i][k] * B.Matrix[k][j]
+				} 
+			}
+		}
+		return C
+	}
+}
+// prints the values on each row of the matrix on a separate line
+// in: the matrix
+func PrintMat(A Matrix) {
+	m := Rows(A)
+	n := Cols(A)
+
+	fmt.Println()
+	for i:= 0; i < m; i++{
+		for j:=0; j < n; j++{
+			fmt.Printf("%g ", A.Matrix[i][j])
+		}
+		fmt.Print("\n")
+	}
+	fmt.Println()
+
 }
