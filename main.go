94eaa7f889c5d1a6660192933482224ba622d571
jn01674361 <joar.nykvist@gmail.com>
2019-07-18 17:59:39 +0200

    Start working on JSON conversions

--- main.go
+++ main.go
@@ -1,28 +1,45 @@
 package main
 
 import(
-    // "fmt"
-    gomatrix "gomatrix"
+    "fmt"
+    // gomatrix "gomatrix"
     //"github.com/gorilla/mux"
-    //"log"
+    "log"
     //"net/http"
-    //"encoding/json"
+    "encoding/json"
 )
 
+type jsonRow struct {
+    Elements []float64
+}
+
+type jsonMatrix struct {
+    Rows []jsonRow
+}
+// func fromJsonArr(jsonData []byte) interface{
+//     var arr interface
+//     err := json.Unmarshal(jsonData, &arr)
+//     if err != nil {
+//         log.Println(err)
+//     }
+//     return arr
+// }
 func main() {
     //router := mux.NewRouter()
+
     test();
 }
 
 func test() {
     
-    E := [][]float64{
-        {1.0, 2.0},
-        {3.0, 4.0},
+    jsonInput := []byte(`[{"Elements": [1.0,2.0]},{"Elements": [3.0, 4.0]}]`)
+
+    var jMat jsonMatrix
+
+    err := json.Unmarshal(jsonInput, &jMat)
+
+    if err != nil{
+        log.Println(err)
     }
-
-    F := gomatrix.ToMatrix(E)
-    
-    gomatrix.PrintMat(F)
-
+    fmt.Println(jMat.Rows)
 }
