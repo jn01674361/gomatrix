a289ff3dd3427eafa103120ebe3b72589e372fcc
jn01674361 <joar.nykvist@gmail.com>
2019-07-21 18:51:19 +0200

    Make API work without parameters

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import(
-    "fmt"
-    // gomatrix "gomatrix"
-    //"github.com/gorilla/mux"
+    // "fmt"
+    gomatrix "gomatrix"
+    "github.com/gorilla/mux"
     "log"
-    //"net/http"
+    "net/http"
     "encoding/json"
 )
 
-type jsonRow struct {
-    Elements []float64
-}
-
-type jsonMatrix struct {
-    Rows []jsonRow
+type matrixPair struct{
+    A [][]float64
+    B [][]float64
 }
 // func fromJsonArr(jsonData []byte) interface{
 //     var arr interface
@@ -25,23 +22,59 @@ type jsonMatrix struct {
 //     return arr
 // }
 func main() {
-    //router := mux.NewRouter()
-
-    test();
+    router := mux.NewRouter()
+    router.HandleFunc("/matmul", matmul).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8000",router))
 }
+func matmul(w http.ResponseWriter, r *http.Request){
+    
+    params := mux.Vars(r)
 
+    log.Println("params")
+    log.Println(params)
+
+    // params will be what holds the matrix!
+    // matmul/{matrix} will need to be routed!
+
+    var mats matrixPair
+    _ = json.NewDecoder(r.Body).Decode(&mats)
+
+    
+    // err := json.Unmarshal(jsonInput, &mats)
+    // if err != nil{
+    //     log.Println(err)
+    // }
+
+    matA := gomatrix.ToMatrix(mats.A)
+    matB := gomatrix.ToMatrix(mats.B)
+    matC := gomatrix.MatMul(matA, matB)
+    
+    json.NewEncoder(w).Encode(matC.Matrix)
+    return 
+    // gomatrix.PrintMat(matA)
+    // gomatrix.PrintMat(matB)
+    // gomatrix.PrintMat(gomatrix.MatMul(matA,matB))
+
+    
+}
 func test() {
     
-    // jsonInput := []byte(`[{"Elements": [1.0,2.0]},{"Elements": [3.0, 4.0]}]`)
 
-    jsonArr := []byte(`[[1.0,2.0],[3.0,4.0]]`)
+    jsonInput := []byte(`{"A":[[1.0,2.0],[3.0,4.0]], "B":[[1.0,2.0],[3.0,4.0]]}`)
 
-    var jArr [][]float64
+    var mats matrixPair
 
-    err := json.Unmarshal(jsonArr, &jArr)
+    err := json.Unmarshal(jsonInput, &mats)
     if err != nil{
         log.Println(err)
     }
-    fmt.Println(jArr)
 
+    matA := gomatrix.ToMatrix(mats.A)
+    matB := gomatrix.ToMatrix(mats.B)
+
+    gomatrix.PrintMat(matA)
+    gomatrix.PrintMat(matB)
+    gomatrix.PrintMat(gomatrix.MatMul(matA,matB))
+
+    //jsonOutput
 }
