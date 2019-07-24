a259e2bc7fbe012af85b0dfa78a4a0cc6cc80911
jn01674361 <joar.nykvist@gmail.com>
2019-07-24 14:52:41 +0200

    Make API calleable

--- main.go
+++ main.go
@@ -8,73 +8,33 @@ import(
     "net/http"
     "encoding/json"
 )
-
 type matrixPair struct{
     A [][]float64
     B [][]float64
 }
-// func fromJsonArr(jsonData []byte) interface{
-//     var arr interface
-//     err := json.Unmarshal(jsonData, &arr)
-//     if err != nil {
-//         log.Println(err)
-//     }
-//     return arr
-// }
+type product struct{
+    result [][]float64
+}
 func main() {
     router := mux.NewRouter()
-    router.HandleFunc("/matmul", matmul).Methods("GET")
+    router.HandleFunc("/matmul/{a}/{b}", matmul).Methods("GET")
     log.Fatal(http.ListenAndServe(":8000",router))
 }
 func matmul(w http.ResponseWriter, r *http.Request){
-    
     params := mux.Vars(r)
-
-    log.Println("params")
-    log.Println(params)
-
-    // params will be what holds the matrix!
-    // matmul/{matrix} will need to be routed!
-
+    byteA := []byte(params["a"])
+    byteB := []byte(params["b"])
     var mats matrixPair
-    _ = json.NewDecoder(r.Body).Decode(&mats)
-
-    
-    // err := json.Unmarshal(jsonInput, &mats)
-    // if err != nil{
-    //     log.Println(err)
-    // }
-
+    err1 := json.Unmarshal(byteA, &mats.A)
+    if err1 != nil{
+        log.Println(err1)
+    }
+    err2 := json.Unmarshal(byteB, &mats.B)
+    if err2!= nil{
+        log.Println(err2)
+    }
     matA := gomatrix.ToMatrix(mats.A)
     matB := gomatrix.ToMatrix(mats.B)
     matC := gomatrix.MatMul(matA, matB)
-    
     json.NewEncoder(w).Encode(matC.Matrix)
-    return 
-    // gomatrix.PrintMat(matA)
-    // gomatrix.PrintMat(matB)
-    // gomatrix.PrintMat(gomatrix.MatMul(matA,matB))
-
-    
-}
-func test() {
-    
-
-    jsonInput := []byte(`{"A":[[1.0,2.0],[3.0,4.0]], "B":[[1.0,2.0],[3.0,4.0]]}`)
-
-    var mats matrixPair
-
-    err := json.Unmarshal(jsonInput, &mats)
-    if err != nil{
-        log.Println(err)
-    }
-
-    matA := gomatrix.ToMatrix(mats.A)
-    matB := gomatrix.ToMatrix(mats.B)
-
-    gomatrix.PrintMat(matA)
-    gomatrix.PrintMat(matB)
-    gomatrix.PrintMat(gomatrix.MatMul(matA,matB))
-
-    //jsonOutput
 }
